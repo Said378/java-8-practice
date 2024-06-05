4d74996cb293209991648aa0cdaba66f8e5ed837
Said <said@gmail.com>
2024-06-06 00:31:35 +0300

    init upload

---
+++ src/test/java/com/practice/java_8/Java8PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.java_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
